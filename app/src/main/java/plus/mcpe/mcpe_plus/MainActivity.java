607575460587310c5dfb397bef91f86df4957e37
Code Hz <codehz@outlook.com>
2015-08-06 17:29:56 +0800

    fix bug
    Signed-off-by: Code Hz <codehz@outlook.com>

--- app/src/main/java/plus/mcpe/mcpe_plus/MainActivity.java
+++ app/src/main/java/plus/mcpe/mcpe_plus/MainActivity.java
@@ -9,10 +9,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -51,10 +53,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupStatusBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
             ImageView imageView = (ImageView) findViewById(R.id.image);
             AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
             imageView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, UiExt.getStatusBarHeight()));
             appBarLayout.setPadding(0, UiExt.getStatusBarHeight(), 0, 0);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
     }
 
@@ -104,9 +109,8 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(Gravity.LEFT);
         }
 
         return super.onOptionsItemSelected(item);
